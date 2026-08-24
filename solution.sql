d1419433898ac62bd0748a50445cf755f1671ee8
SANJAY R <sanjay.ganapathy.7200@gmail.com>
2026-08-24 11:28:28 +0530

    PROGRAM 1

--- solution.sql
+++ solution.sql
@@ -1,1 +1,10 @@
+create database CollegeDB;
+use CollegeDB;
 
+create table Department (
+    DepartmentID INT(5) PRIMARY KEY,
+    DepartmentName VARCHAR(20),
+    HOD VARCHAR(20)
+);
+
+describe Department;
